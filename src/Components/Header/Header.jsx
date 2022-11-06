f2bfd756152bf476e6128727eb66d3e4d3b1f753
Nargiza <saparbaeva.nargiza@gmail.com>
2022-11-07 02:30:56 +0600

    fix: adding header logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from "../../logo.svg"
 import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
         <div className="header">
             <Link to="/">
-                <img className="logo" src={logo} alt="rfhnbyf"/>
+                <img className="logo" src="https://www.pngall.com/wp-content/uploads/13/NFT-No-Background.png" alt="rfhnbyf"/>
                 <span className="logo_title">NFT GALLERY</span>
             </Link>
 
